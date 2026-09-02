dbdc8b94fc7fea6e1c487c8cf6c14701ca445a36
Hassan Fasih <hassan.fasih131@gmail.com>
2026-09-02 03:21:32 -0700

    Created a board for displaying

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-
+import sys
+import requests
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import Qt
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp() #weather app object
+    weather_app.show() #showing weather app
+    sys.exit(app.exec()) #lets the app stay until its closed
