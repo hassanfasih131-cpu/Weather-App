8feff313dc394040793ac09ca511b6ba190783c2
Hassan Fasih <hassan.fasih131@gmail.com>
2026-09-03 06:16:43 -0700

    Adding labels in display

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import sys
 import requests
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
 from PyQt6.QtCore import Qt
 class WeatherApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.city_label=QLabel("Enter the city name: ",self)
+        self.city_input=QLineEdit(self)
+        self.get_weather_button=QPushButton("Get Weather",self)
+        self.temperature_label=QLabel("70 degrees F ",self)
+        self.emoji_label=QLabel("*",self)
+        self.description_label=QLabel("It is sunny: ",self)
+        self.init_ui()
+    def init_ui(self):
+        pass
 if __name__=="__main__":
     app = QApplication(sys.argv)
     weather_app = WeatherApp() #weather app object
